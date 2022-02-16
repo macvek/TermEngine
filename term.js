95fd76a700cd431488abb8db84cc44a04e150e68
Maciej Aleksandrowicz <macvek@gmail.com>
2022-02-16 23:07:47 +0100

    initial commit; some basic terminal rendering

---
+++ term.js
@@ -0,0 +1,192 @@
+window.addEventListener('load', start);
+
+var term = {
+    BLACK       : 30,        GRAY        :90,
+    RED         : 31,        LIGHTRED    :91,
+    GREEN       : 32,        LIGHTGREEN  :92,
+    YELLOW      : 33,        LIGHTYELLOW :93,
+    BLUE        : 34,        LIGHTBLUE   :94,
+    MAGENTA     : 35,        LIGHTMAGENTA:95,
+    CYAN        : 36,        LIGHTCYAN   :96,
+    LIGHTGRAY   : 37,        WHITE       :97
+};
+
+var ansiToCSS = {};
+(function() {
+    var a = ansiToCSS;
+    a[term.BLACK]       = 'ansi-black';    a[term.GRAY]        = 'ansi-gray';
+    a[term.RED]         = 'ansi-red';      a[term.LIGHTRED]    = 'ansi-lightred';
+    a[term.GREEN]       = 'ansi-green';    a[term.LIGHTGREEN]  = 'ansi-lightgreen';
+    a[term.YELLOW]      = 'ansi-yellow';   a[term.LIGHTYELLOW] = 'ansi-lightyellow';
+    a[term.BLUE]        = 'ansi-blue';     a[term.LIGHTBLUE]   = 'ansi-lightblue';
+    a[term.MAGENTA]     = 'ansi-magenta';  a[term.LIGHTMAGENTA]= 'ansi-lightmagenta';
+    a[term.CYAN]        = 'ansi-cyan';     a[term.LIGHTCYAN]   = 'ansi-lightcyan';
+    a[term.LIGHTGRAY]   = 'ansi-lightgray';a[term.WHITE]       = 'ansi-white';
+    
+}());
+
+window.term = term;
+
+function start() {
+    build80x24CharacterMap();
+    var buffer = consoleBuffer();
+    var handler = consoleHandler();
+    var cursor = buildCursor();
+
+    TermWrite(buffer, 1,1,"Hello #>");
+    TermWrite(buffer, 1,2,">> Welcome to THEME PARK << ");
+    TermFlush(handler, buffer);
+    TermCursorTo(cursor, handler, buffer, 1,1);
+    TermBlink(cursor, 500);
+}
+
+function TermWrite(buffer,iX,iY,text, textColors=[]) {
+    var x = iX -1;
+    var y = iY -1;
+
+    var chars = buffer.chars;
+    var colors = buffer.colors;
+    if (y >= chars.length || y < 0) {
+        return;
+    }
+
+    var line = chars[y];
+    var colorLine = colors[y];
+    var topX = Math.min(line.length, x + text.length);
+
+    for (var i=x;i<topX;i++) {
+        line[i] = text.charAt(i-x);
+        if (textColors[0]) {
+            colorLine[i][0] = textColors[0]; 
+        }
+        if (textColors[1]) {
+            colorLine[i][1] = textColors[1];
+        }
+    }
+  
+}
+
+function TermFlush(handler, buffer) {
+    var chars = buffer.chars;
+    var colors = buffer.colors;
+
+    for (var y=0;y<24;y++) {
+        for (var x=0;x<80;x++) {
+            var each = handler[y][x];
+            each.innerHTML = chars[y][x];
+            each.className = cssClassNameFromAnsii(colors[y][x]);
+        }
+    }
+}
+
+function TermCursorTo(cursor, handler, buffer, x,y) {
+    var charBox = handler[y][x];
+    var rect = charBox.getBoundingClientRect();
+    
+    var px = 'px';
+
+    var cursorRatio = 0.1;
+
+    var cursorHeight = Math.max(1, rect.height*cursorRatio);
+
+    cursor.style.width = rect.width + px;
+    cursor.style.height = cursorHeight + px;
+    cursor.style.left = rect.x + px;
+    cursor.style.top = rect.height - cursorHeight + rect.y + px;
+    
+    var frontColor = buffer.colors[y][x][0];
+    
+    cursor.className = cssClassNameFromAnsii( [frontColor, frontColor]);
+
+}
+
+function TermBlink(what, delay) {
+    setInterval(function() {
+        if (what.style.visibility == 'hidden') {
+            what.style.visibility = 'visible';
+        }
+        else {
+            what.style.visibility = 'hidden';
+        }
+    }, delay);
+}
+
+function cssClassNameFromAnsii(frontAndBackColor) {
+    return "FG-"+ansiToCSS[frontAndBackColor[0]]+" "+"BG-"+ansiToCSS[frontAndBackColor[1]];
+}
+
+function randomColor() {
+    var colors = [term.BLACK, term.GRAY, term.RED, term.LIGHTRED, term.GREEN, 
+        term.LIGHTGREEN, term.YELLOW, term.LIGHTYELLOW, term.BLUE, term.LIGHTBLUE, 
+        term.MAGENTA, term.LIGHTMAGENTA, term.CYAN, term.LIGHTCYAN, term.LIGHTGRAY, term.WHITE];
+
+    return colors[Math.floor(Math.random()*colors.length)];
+}
+
+function build80x24CharacterMap() {
+    var term = document.getElementById("term");
+    for (var y=0;y<24;y++) {
+        for (var x=0;x<80;x++) {
+            term.appendChild(createCharacter(x,y));
+        }
+        term.appendChild(createNewLine());
+    }
+}
+
+function createCharacter(x,y) {
+    var character = document.createElement("span");
+    character.setAttribute("id", characterIdFor(x,y));
+    character.innerHTML = ' ';
+    return character;
+}
+
+function createNewLine() {
+    var newLine = document.createTextNode("\n");
+    return newLine;
+}
+
+function characterIdFor(x,y) {
+    return "term-char-"+x+"x"+y;
+}
+
+function consoleHandler() {
+    var lines = [];
+    for (var y=0;y<24;y++) {
+        var row = [];
+        lines.push(row);
+        for (var x=0;x<80;x++) {
+            row.push(document.getElementById(characterIdFor(x,y)));
+        }
+    }
+
+    return lines;
+}
+
+function buildCursor() {
+    var cursor = document.createElement("div");
+    cursor.setAttribute('id', 'cursor');
+    document.getElementById('term').appendChild(cursor);
+    cursor.style.position = 'absolute';
+
+    return cursor;
+    
+}
+
+function consoleBuffer() {
+    var characters = [];
+    var colors = [];
+
+    for (var y=0;y<24;y++) {
+        var line = [];
+        var colorLine = [];
+        for (var x=0;x<80;x++) {
+            line.push(' ');
+            colorLine.push([term.WHITE,term.BLACK]);
+        }
+        characters.push(line);
+        colors.push(colorLine);
+    }
+
+    return {chars: characters, colors:colors}
+}
+
